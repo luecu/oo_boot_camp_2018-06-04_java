bcf1d719c4ace9640639256c3eedf2d085895489
Fred George <fredgeorge@acm.org>
2018-06-06 16:54:42 +0200

    Fred: Graph with canReach()

---
+++ src/graph/Node.java
@@ -0,0 +1,37 @@
+package graph;
+/*
+ * Copyright (c) 2018 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Understands its neighbors
+public class Node {
+    private final List<Node> neighbors = new ArrayList<>();
+
+    public Node to(Node neighbor) {
+        neighbors.add(neighbor);
+        return neighbor;
+    }
+
+    public boolean canReach(Node destination) {
+        return this.canReach(destination, noVisitedNodes());
+    }
+
+    private boolean canReach(Node destination, List<Node> visitedNodes) {
+        if (this == destination) return true;
+        if (visitedNodes.contains(this)) return false;
+        visitedNodes.add(this);
+        for(Node n: neighbors) {
+            if (n.canReach(destination, visitedNodes)) return true;
+        }
+        return false;
+    }
+
+    private List<Node> noVisitedNodes() {
+        return new ArrayList<>();
+    }
+}
